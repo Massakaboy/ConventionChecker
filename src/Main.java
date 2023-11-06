e6d1487c4a697eb55193dc06990f26976df10d3b
Friedinger <71267611+Friedinger@users.noreply.github.com>
2023-11-06 12:41:05 +0100

    Add author and version comment

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 /*
 	Convention Checker
 	https://github.com/Friedinger/ConventionChecker
+
+	@author Friedinger https://github.com/Friedinger
+	@author IamPekka058 https://github.com/IamPekka058
+	@author Massakaboy https://github.com/Massakaboy
+
+	@version 0.1
  */
 public class Main {
 	public static void main(String[] args) {
